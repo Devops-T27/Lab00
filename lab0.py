ee1119eb69185bbd25e63558ee5e9cbe78b0aab8
Devops-T27 <89909793+Devops-T27@users.noreply.github.com>
2022-02-23 05:43:02 -0500

    code1

--- lab0.py
+++ lab0.py
@@ -1,1 +1,12 @@
+#list of all types
+list=[5,20,12,58,58,5,2,63,9,3,1,78,47,15,"hello5",12.5,"yo",20]
 
+
+#should return the int or float content number 5
+def get5(list):
+    #code here ...
+    pass
+
+
+#show new list
+print(get5(list))
